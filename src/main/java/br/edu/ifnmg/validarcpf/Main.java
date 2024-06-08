54b5e6d34abd7d03823e17699db2d25f6aab081b
Gustavo <gustavo.l.mav@gmail.com>
2024-06-08 10:44:10 -0300

    criando main

---
+++ src/main/java/br/edu/ifnmg/validarcpf/Main.java
@@ -0,0 +1,11 @@
+package br.edu.ifnmg.validarcpf;
+
+/**
+ * 
+ * @author Gustavo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/br/edu/ifnmg/validarcpf/ValidarCpf.java
@@ -0,0 +1,10 @@
+package br.edu.ifnmg.validarcpf;
+
+/**
+ * Classe utilizada para verificar se um CPF é válido.
+ * @author Gustavo
+ */
+public class ValidarCpf {
+
+
+}
