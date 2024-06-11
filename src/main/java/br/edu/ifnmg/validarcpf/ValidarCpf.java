6ad32cfe4d37d33abcb54c53dc24874bd2983909
Gustavo <gustavo.l.mav@gmail.com>
2024-06-11 15:39:47 -0300

    metodo obterDigito

--- src/main/java/br/edu/ifnmg/validarcpf/ValidarCpf.java
+++ src/main/java/br/edu/ifnmg/validarcpf/ValidarCpf.java
@@ -5,6 +5,21 @@ package br.edu.ifnmg.validarcpf;
  * @author Gustavo
  */
 public class ValidarCpf {
-
+    /**
+     * Metodo para obter cada digito do numero obtido.
+     * @param cpf
+     * @param pos
+     * @return int
+     */
+    static int obterDigito(Long cpf, int pos){
+        int div = 10; int mod = 10;
+        Double digito = (cpf.doubleValue()
+                        %(Math.pow(mod, pos))
+                        /(Math.pow(div, pos-1)));
+        //Digito recebe o wrapper Double, por conta dos metodos Math
+        //retornarem double
+        
+        return digito.intValue(); 
+    }
 
 }
