3cd2adfa7c1cf12a1d8e0377bfe318dc6411c67a
Gustavo <gustavo.l.mav@gmail.com>
2024-06-11 16:49:13 -0300

    metodo isCpfValido String

--- src/main/java/br/edu/ifnmg/validarcpf/Main.java
+++ src/main/java/br/edu/ifnmg/validarcpf/Main.java
@@ -6,7 +6,19 @@ package br.edu.ifnmg.validarcpf;
  */
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println(ValidarCpf.obterDigito(14230703058L,3));
+        ValidarCPFCNPJ cpf = new ValidarCPFCNPJ();
+        try {
+            cpf.setCpf_long(456789014L);
+            System.out.println("Valido");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            cpf.setCpf_String("002.307.030-58");
+            System.out.println("Valido");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/br/edu/ifnmg/validarcpf/ValidarCPFCNPJ.java
+++ src/main/java/br/edu/ifnmg/validarcpf/ValidarCPFCNPJ.java
@@ -4,7 +4,37 @@ package br.edu.ifnmg.validarcpf;
  * Classe utilizada para verificar se um CPF é válido.
  * @author Gustavo
  */
-public class ValidarCpf {
+public class ValidarCPFCNPJ {
+    long cpf_long;
+    String cpf_String;
+    
+    //<editor-fold defaultstate="collapsed" desc="Set e Get CPF">
+    
+    public double getCpf_long() {
+        return cpf_long;
+    }
+
+    public void setCpf_long(long cpf_long)  throws Exception{
+        if(!ValidarCPFCNPJ.isCPFValido(cpf_long)){
+            throw new Exception("Invalido");
+        }
+        this.cpf_long = cpf_long;
+    }
+
+    public String getCpf_String() {
+        return cpf_String;
+    }
+
+    public void setCpf_String(String cpf_String) throws Exception{
+        if(!ValidarCPFCNPJ.isCPFValido(cpf_String)){
+            throw new Exception("Invalido");
+        }
+        this.cpf_String = cpf_String;
+    }
+    //</editor-fold>
+    
+    
+    
     /**
      * Metodo para obter cada digito do numero obtido.
      * @param cpf
@@ -20,6 +50,67 @@ public class ValidarCpf {
         //retornarem double
         
         return digito.intValue(); 
+    }
+    
+    /**
+    *Esse metodo faz a contagem de digitos, caso tenha +11 digitos
+    *o CPF é invalido.
+    * @param cpf
+    * @return int
+    */
+    static int contagemDigito(Long cpf){
+        int contador = 0;
+        while(cpf >= 1){
+            cpf /= 10;
+            contador++;
+            
+        }   
+        return contador;
+    }
+    
+    /**
+     * Método que utiliza os calculos para validar um CPF.
+     * @param cpf
+     * @return bool
+     */
+    static boolean isCPFValido(long cpf){
+        int digitos = ValidarCPFCNPJ.contagemDigito(cpf);
+        int soma = 0;
+        
+        if(digitos>11)
+            return false;
+        
+        for(int i = digitos; i > 2; i--){
+            soma += (ValidarCPFCNPJ.obterDigito(cpf, i)*(i-1));
+        }
+
+        if(11-(soma%11)==10)
+            soma = 0;
+        if((11-(soma%11))!= ValidarCPFCNPJ.obterDigito(cpf, 2))
+            return false;
+        
+        soma = 0;
+        for(int i = digitos; i > 1; i--)
+            soma += (ValidarCPFCNPJ.obterDigito(cpf, i)*i);
+        
+        
+        if(11-(soma%11)==10)
+            soma = 0;
+        
+        return (11-(soma%11)) == ValidarCPFCNPJ.obterDigito(cpf, 1);
+    }
+    
+    /**
+     * Método Sobrecarregado de validar o CPF, utilizando String de parametro
+     * @param cpf
+     * @return 
+     */
+    static boolean isCPFValido(String cpf){
+        cpf = cpf.replace(".", "").replace("-", "");
+        if(ValidarCPFCNPJ.isCPFValido(Long.parseLong(cpf))){
+            return true;
+        }
+        return false;
     }
 
 }
